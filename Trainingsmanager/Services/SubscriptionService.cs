03ff4a5a217490cabd24b8057c5862b0bf283ada
Morten Steitz <morten_1995@hotmail.de>
2026-08-21 13:20:52 +0200

    remove restriction to only add subscription 3 days before the start

--- Trainingsmanager/Services/SubscriptionService.cs
+++ Trainingsmanager/Services/SubscriptionService.cs
@@ -125,15 +125,16 @@ namespace Trainingsmanager.Services
             //    }
             //}
 
-            var timeIn3Days = DateTime.UtcNow.AddHours(72);
+            // Vorerst wieder raus
+            //var timeIn3Days = DateTime.UtcNow.AddHours(72);
 
-            if (session.TrainingStart > timeIn3Days)
-            {
-                if (!hasAdminRole)
-                {
-                    throw new Exception("Eine Anmeldung ist erst 3 Tage vor Beginn der Session möglich.");
-                }
-            }
+            //if (session.TrainingStart > timeIn3Days)
+            //{
+            //    if (!hasAdminRole)
+            //    {
+            //        throw new Exception("Eine Anmeldung ist erst 3 Tage vor Beginn der Session möglich.");
+            //    }
+            //}
 
             // Only Admins are allowed add Subscriptions for MitgliederOnlySession
             if (session.MitgliederOnlySession)
